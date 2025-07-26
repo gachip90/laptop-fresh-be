441fe9360b8b7e71967e355394ce75a2f5f2b94a
GaChip <gachip9090@gmail.com>
2025-07-26 16:45:29 +0700

    fix cors error

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://laptop-fresh-fe.vercel.app/', 'http://localhost:3000']
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/uploads', express.static('uploads'));
